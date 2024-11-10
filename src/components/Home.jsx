e36de049d345290adcb7d52a4a4451ac707d2463
MaxxKalashnikov <max.vanholl@mail.ru>
2024-11-10 22:05:37 +0200

    Modifed home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,28 +6,34 @@ const tmdbKey = import.meta.env.VITE_TMDB_API_KEY
 const Home = () => {
     const [movieList, setMovieList] = useState([])
     useEffect(() => {
-        const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
+        // test of the API, works only for this partcular endpoint
+        const urls = [1, 2, 3].map(
+            page => `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${page}`
+          );  
+
         const options = {
-        method: 'GET',
-        headers: {
+          method: 'GET',
+          headers: {
             accept: 'application/json',
             Authorization: `Bearer ${tmdbKey}`
-        }
+          }
         };
-
-        fetch(url, options)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data.results)
-            setMovieList(data.results)
-        })
-        .catch(err => console.error(err));
+        //have to use promise all as the endpoint provides 
+        //info in 3 different urls 
+        Promise.all(urls.map(url => fetch(url, options).then(res => res.json())))
+            .then((responses) => {
+                const allMovies = responses.flatMap(response => response.results);
+                setMovieList(movieList.concat(allMovies))
+            })
+        .catch((error) => console.error(error));
     }, [])
+    
     const images = [
         '/src/assets/pic1.jpg',
         '/src/assets/pic2.jpg',
         '/src/assets/pic3.jpg'
       ];
+      
     return (
         <>
             <h2>Home Page</h2>
--- src/components/PopularMovieList.jsx
+++ src/components/PopularMovieList.jsx
@@ -1,7 +1,7 @@
 const PopularMovieList = ({movieList, setMovieList}) => {
     return (
         <>
-            {movieList.map(item => <img src={`https://image.tmdb.org/t/p/w300${item.backdrop_path}`} alt="Movie Poster" />)}
+            {movieList.map(item => <img key={item.id} src={`https://image.tmdb.org/t/p/w300${item.backdrop_path}`} alt="Movie Poster" />)}
         </>
     )
 }
