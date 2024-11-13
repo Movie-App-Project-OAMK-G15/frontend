3687faa95aec7374182ce6df5ddf2258aa64ca5b
MaxxKalashnikov <max.vanholl@mail.ru>
2024-11-13 15:36:18 +0200

    slight UI improvment

--- src/screens/UserAccount.jsx
+++ src/screens/UserAccount.jsx
@@ -3,14 +3,20 @@ import Navbar from "../components/Navbar"
 
 export default function UserAccount(){
     const { user, logOut, deleteAccount } = useUser()
-
+    function handleDelete(){
+        if (confirm("Are you sure you want to delete this account?")) {
+            deleteAccount();
+        } else {
+            console.log("User canceled the action.");
+        }
+    }
     return (
         <>
             <Navbar/>
             <div>{user.firstname}</div>
             <div>{user.familyname}</div>
             <div>{user.email}</div>
-            <button onClick={deleteAccount}>Delete account</button>
+            <button onClick={handleDelete}>Delete account</button>
             <button onClick={logOut}>Log Out</button>
         </>
     )
